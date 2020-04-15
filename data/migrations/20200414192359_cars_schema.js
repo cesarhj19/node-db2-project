688a0df54f97e1ddf1c1c7a74d38ef968028b0a2
cesarhj19 <cesarhj19@gmail.com>
2020-04-14 19:27:39 -0700

    Added unique constraint to vin

--- data/migrations/20200414192359_cars_schema.js
+++ data/migrations/20200414192359_cars_schema.js
@@ -3,7 +3,7 @@
 exports.up = function (knex) {
   return knex.schema.createTable('cars', (tbl) => {
     tbl.increments('id');
-    tbl.string('VIN', 17).notNullable();
+    tbl.string('VIN', 17).notNullable().unique();
     tbl.string('make', 128).notNullable();
     tbl.string('model', 128).notNullable();
     tbl.integer('mileage').notNullable();
